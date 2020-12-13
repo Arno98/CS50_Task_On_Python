4859fbff6929229cb8b66da8a149002ac08e614b
Roma Pavlov <49715263+Arno98@users.noreply.github.com>
2020-12-13 23:17:54 +0200

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,24 +1,42 @@
 def vigenere():
 	alphavite = 'abcdefghijklmnopqrstuvwxyz'
-	word_key = 'abc'
-	open_text = 'hello'
+	alphavite_2 = alphavite.upper()
+	word_key = str(input("Word key: "))
+	open_text = str(input("Open text: "))
 	closed_text = ''
 	
 	char_open_text = 0
 	char_word_key = 0
 	
 	while char_open_text != len(open_text):
-		for c in open_text:
-			if c in alphavite:
+		
+		if open_text[char_open_text] not in alphavite and open_text[char_open_text] not in alphavite_2:
+			closed_text += open_text[char_open_text]
+			char_open_text += 1
+			
+		else:
+			if open_text[char_open_text] in alphavite:
 				offset = (alphavite.index(open_text[char_open_text]) + alphavite.index(word_key[char_word_key])) % 26
 				for char in alphavite:
 					if alphavite.index(char) == offset:
 						closed_text += char
 				char_open_text += 1
 				char_word_key += 1
+			
 				if char_word_key == len(word_key):
 					char_word_key -= len(word_key)
+					
+			elif open_text[char_open_text] in alphavite_2:
+				offset = (alphavite_2.index(open_text[char_open_text]) + alphavite.index(word_key[char_word_key])) % 26
+				for char in alphavite_2:
+					if alphavite_2.index(char) == offset:
+						closed_text += char
+				char_open_text += 1
+				char_word_key += 1
 			
+				if char_word_key == len(word_key):
+					char_word_key -= len(word_key)
+					
 	print(open_text)
 	print(closed_text)	
 		
