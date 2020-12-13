49bae8f90b779cda6715cda2cf2c43356a6766af
Roma Pavlov <49715263+Arno98@users.noreply.github.com>
2020-12-13 23:25:10 +0200

    Add files via upload

---
+++ mario_hard.py
@@ -0,0 +1,8 @@
+while True:
+	height = input("Height: ")
+	if height.isdigit():
+		height = int(height)
+		if height > 0 and height <= 8:
+			for x in range(height):
+				print((" " * (height - (x+1))) + ("#" * (x+1)) + (" " * 2) + ("#" * (x+1)))
+			break
